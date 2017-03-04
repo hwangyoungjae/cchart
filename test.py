8eaa1ffd97d17061cb697f916e5d057f41744f90
Youngjae <dudwo56@gmail.com>
2017-03-04 11:43:54 +0900

    ac

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 import os,sys,time,datetime
 import pandas_datareader.data
 from pandas import Series, DataFrame
-import json, math
+import json, math, MySQLdb
 
 # daeshin = {'open':  [11650, 11100, 11200, 11100, 11000],
 #            'high':  [12100, 11800, 11200, 11100, 11150],
@@ -16,6 +16,21 @@ import json, math
 # daeshin_day = DataFrame(daeshin, columns=['open', 'high', 'low', 'close'], index=date)
 # print daeshin_day
 
+
+con = MySQLdb.connect('192.168.0.15','root','Admin2013!','cchart')
+cur = con.cursor()
+
+cur.execute('SELECT * FROM web_code;')
+print cur._executed
+for row in cur.fetchall():
+    CODE, = row
+    cur.execute('SELECT COUNT(1) FROM web_stock WHERE code_id=%s;',(CODE,))
+    print cur._executed
+    if not cur.fetchone()[0]:
+        cur.execute('DELETE FROM web_code WHERE code=%s;',(CODE,))
+        print cur._executed
+con.commit()
+con.close()
 
 
 
